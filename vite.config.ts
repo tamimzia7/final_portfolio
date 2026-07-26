a332c4bd97b282fbcc1b09faa274e72a4cdb1d86
tamimzia7 <tamimweb27@gmail.com>
2026-07-26 11:50:52 +0600

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  // GitHub Pages deploys to https://<user>.github.io/final_portfolio/
-  base: "/final_portfolio/",
   plugins: [react()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
